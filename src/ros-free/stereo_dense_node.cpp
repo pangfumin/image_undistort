bd0c9ff9b58ced7d23679d6ee1196200290cca4f
pangfumin <pangfumin@gmai.com>
2019-06-13 22:33:18 +0800

    load camera parameter

--- src/ros-free/stereo_dense_node.cpp
+++ src/ros-free/stereo_dense_node.cpp
@@ -3,12 +3,6 @@
 
 #include <yaml-cpp/yaml.h>
 
-//const cv::Size& resolution,
-//const Eigen::Matrix<double, 4, 4>& T,
-//const Eigen::Matrix<double, 3, 3>& K,
-//const std::vector<double>& D,
-//const DistortionModel& distortion_model
-
 void loadCameraParamsFromYamlNode(const YAML::Node& node, cv::Size& res,
                                   Eigen::Matrix<double, 4, 4>& T,
                                   Eigen::Matrix<double, 3, 3>& K,
@@ -23,35 +17,39 @@ void loadCameraParamsFromYamlNode(const YAML::Node& node, cv::Size& res,
     YAML::Node resolution = node["resolution"];
     res = cv::Size(resolution[0].as<int>(), resolution[1].as<int>());
 
+    YAML::Node intrinsics = node["intrinsics"];
+    K << intrinsics[0].as<double>(), 0, intrinsics[2].as<double>(),
+            0, intrinsics[0].as<double>(1), intrinsics[3].as<double>(),
+                    0,0,1;
+    if (node["T_cn_cnm1"].IsDefined()) {
+        for (int i = 0; i < 4; i++ ) {
+            for (int j = 0; j < 4; j++) {
+                T(i,j) = node["T_cn_cnm1"][i][j].as<double>();
+            }
+        }
 
-
-//    for (std::size_t i=0;i<primes.size();i++) {
-//        std::cout << primes[i].as<float>() << "\n";
-//    }
+    } else {
+        T.setIdentity();
+    }
 
 }
 int main (int argc, char** argv) {
-    std::string fin = "/media/pang/Plus/segway_outdoor/cui_stereo_calib/camchain-cam_stereo.yaml";       //yaml文件所在的路径
+    std::string fin = "/home/pang/data/dataset/segway_outdoor/cui_stereo_calib/camchain-cam_stereo.yaml";
     YAML::Node yamlConfig = YAML::LoadFile(fin);
-
-
-    const std::string username = yamlConfig["cam0"]["camera_model"].as<std::string>();
-    std::cout << "username: " << username << std::endl;
-    YAML::Node node  = yamlConfig["cam0"];
-    std::cout << node["distortion_coeffs"].Type() << std::endl;
-//    std::vector<float> = node["distortion_coeffs"].as<std::vector<float>>() ;
+    YAML::Node node0  = yamlConfig["cam0"];
+    YAML::Node node1  = yamlConfig["cam1"];
 
     image_undistort::CameraParametersPair left_camera_param_pair;
     image_undistort::CameraParametersPair right_camera_param_pair;
-//    left_camera_param_pair.setInputCameraParameters()
 
 
-   cv::Size resolution;
-   Eigen::Matrix<double, 4, 4> T;
-   Eigen::Matrix<double, 3, 3> K;
-   std::vector<double> D;
-    image_undistort::DistortionModel distortion_model;
-    loadCameraParamsFromYamlNode(node, resolution, T, K, D, distortion_model);
+   cv::Size resolution0, resolution1;
+   Eigen::Matrix<double, 4, 4> T0, T1;
+   Eigen::Matrix<double, 3, 3> K0, K1;
+   std::vector<double> D0, D1;
+    image_undistort::DistortionModel distortion_model0, distortion_model1;
+    loadCameraParamsFromYamlNode(node0, resolution0, T0, K0, D0, distortion_model0);
+    loadCameraParamsFromYamlNode(node1, resolution1, T1, K1, D1, distortion_model1);
 
 
 
